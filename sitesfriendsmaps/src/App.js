235aaa8ae85f2993dd9653fd32667dd95600b6c7
master <marcos1003@hotmail.com>
2019-10-23 20:05:23 -0300

    .

--- sitesfriendsmaps/src/App.js
+++ sitesfriendsmaps/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import firebase from 'firebase'
 
 export default class App extends Component {
   constructor(props) {
@@ -7,6 +8,26 @@ export default class App extends Component {
       email: "",
       password: ""
     }
+
+    var firebaseConfig = {
+      apiKey: "AIzaSyBX2Vu7naENdJUGEzC1NiPfPWn6LVh4Vi4",
+      authDomain: "friendsmaps-5198b.firebaseapp.com",
+      databaseURL: "https://friendsmaps-5198b.firebaseio.com",
+      projectId: "friendsmaps-5198b",
+      storageBucket: "friendsmaps-5198b.appspot.com",
+      messagingSenderId: "910339080484",
+      appId: "1:910339080484:web:2e02ff995f52b42721f3b1",
+      measurementId: "G-V4VPNWYBWM"
+    };
+    firebase.initializeApp(firebaseConfig);
+  }
+
+  salvarUsuario() {
+
+    
+  }
+  login() {
+
   }
 
 
@@ -33,8 +54,8 @@ export default class App extends Component {
 
         />
         <br />
-        <button>ENTRAR</button>
-        <button>NOVO USUÁRIO</button>
+        <button onClick={() => this.login()}>ENTRAR</button>
+        <button onClick={() => this.salvarUsuario()}>NOVO USUÁRIO</button>
 
       </div>
     )
