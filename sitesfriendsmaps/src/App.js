0fce1e673c75b2a1ab9d44d45cdb366248c4beac
master <marcos1003@hotmail.com>
2019-10-23 20:00:25 -0300

    .

--- sitesfriendsmaps/src/App.js
+++ sitesfriendsmaps/src/App.js
@@ -1,10 +1,41 @@
 import React, { Component } from 'react'
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      email: "",
+      password: ""
+    }
+  }
+
+
   render() {
     return (
-      <div>
-        Olá Mundo
+      <div style={{ textAlign: 'center' }}>
+        <h1>LOGIN</h1>
+        <input
+          placeholder="Informe o seu e-mail"
+          type="email"
+          value={this.state.email}
+          onChange={
+            (e) => this.setState({ email: e.target.value })
+          }
+        />
+        <br />
+        <input
+          placeholder="Informe sua senha"
+          type="password"
+          value={this.state.password}
+          onChange={
+            (e) => this.setState({ password: e.target.value })
+          }
+
+        />
+        <br />
+        <button>ENTRAR</button>
+        <button>NOVO USUÁRIO</button>
+
       </div>
     )
   }
