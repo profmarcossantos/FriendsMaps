f172b46662b7b6e475ddff6892bb66bbb8f8dd1f
master <marcos1003@hotmail.com>
2019-10-23 20:48:40 -0300

    .

--- sitesfriendsmaps/src/pages/menu.js
+++ sitesfriendsmaps/src/pages/menu.js
@@ -3,8 +3,21 @@ import React, { Component } from 'react'
 export default class Menu extends Component {
     render() {
         return (
-            <div>
-                Estamos no Menu
+            <div style={{textAlign: "center"}}>
+                <h1>Lista de Amigos</h1>
+                <table border="true" >
+                    <tr>
+                        <td>NOME</td>
+                        <td>ENDEREÇO</td>
+                        <td>E-MAIL</td>
+                    </tr>
+                    <tr>
+                        <td>Pedro</td>
+                        <td>Rua Prudente de Morais, 296 Carazinho</td>
+                        <td>pedroca@gmail.com</td>
+                    </tr>
+                    
+                </table>
             </div>
         )
     }
